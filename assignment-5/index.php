b751b874022acfac3c66e76ae0031fd878f136dc
rehanmosabbir <rehanmosabbir@gmail.com>
2023-02-27 16:28:05 +0600

    delete personInfo.php, submit.php and modify index.php

--- assignment-5/index.php
+++ assignment-5/index.php
@@ -8,23 +8,71 @@
     <title>Document</title>
   </head>
   <body>
-    <div class="container mt-5">
-      <form action="submit.php" method="post" class="col-sm-6 bg-light  p-4">
-        <div class="row mb-3">
-          <label for="name" class="col-sm-3 col-form-label">Name: </label>
-          <div class="col-sm-9">
-            <input type="text" name="name" id="name" class="form-control"/>
-          </div>
-        </div>
-        <div class="row mb-3">
-          <label for="email" class="col-sm-3 col-form-label">Email Address: </label>
-          <div class="col-sm-9">
-            <input type="email" name="email" id="email" class="form-control"/>
-          </div>
-        </div>
+    <div class="container-fluid mt-5">
+      <div class="row">
+        <div  class="col-sm-5 bg-light p-4 mx-2 border border-1">
+          <form action="index.php" method="post">
+            <div class="row mb-3">
+              <label for="name" class="col-sm-3 col-form-label">Name: </label>
+              <div class="col-sm-9">
+                <input type="text" name="name" id="name" class="form-control"/>
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="email" class="col-sm-3 col-form-label">Email Address: </label>
+              <div class="col-sm-9">
+                <input type="email" name="email" id="email" class="form-control"/>
+              </div>
+            </div>
 
-        <input type="submit" value="submit" class="btn btn-info text-white" />
-      </form>
+            <input type="submit" value="submit" class="btn btn-info text-white" />
+          </form>
+        </div>
+        <?php
+include "person.php";
+$person = new Person( "Md. Mosabbir Hossain", "rehanmosabbir@gmail.com" );
+$person->setName( "Rezwana Sarmin" );
+$person->setEmail( "rokti.red@gmail.com" );
+?>
+        <div class="col-md-5 bg-dark text-white p-4 mx-2">
+          <h1 class="h1">Person's Information</h1>
+          <hr>
+          <p>Name: <?php echo $person->getName(); ?></p>
+          <p>Email: <?php echo $person->getEmail(); ?></p>
+        </div>
+      </div>
+      <?php
+
+if ( !empty( $_POST ) ) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $personNew = new Person( $name, $email );
+    $personNew->setName( $name );
+    $personNew->setEmail( $email );
+
+}
+
+?>
+      <div class="row">
+        <div class="col-md-5 mx-2 mt-5 bg-dark text-white p-4">
+            <h1 class="h1">Person's Information</h1>
+            <hr>
+            <p>Name: <?php
+
+if ( !empty( $_POST ) ) {
+    echo $personNew->getName();
+}
+
+?></p>
+            <p>Email: <?php
+
+if ( !empty( $_POST ) ) {
+    echo $personNew->getEmail();
+}
+
+?></p>
+        </div>
+      </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
--- assignment-5/personInfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "person.php";
-$person = new Person( "Md. Mosabbir Hossain", "rehanmosabbir@gmail.com" );
-$person->setName( "Rezwana Sarmin" );
-$person->setEmail( "rokti.red@gmail.com" );
-
-echo "<h2 class='h1'>Person Information:</h2>";
-echo "Name: " . $person->getName() . "<br>";
-echo "Email: " . $person->getEmail() . "<br><br>";
